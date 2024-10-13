bd718c7348868c9d24f39641c46a0d2c28020601
Pratheep | MCA <129410816+Pratheep-T2003@users.noreply.github.com>
2024-10-13 14:02:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import { boston } from "./boston.js";
+
+let data = boston.data;
+let top = [];
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i][11] >  200000) {
+    top.push({ name: data[i][8], salary: data[i][11] });
+  }
+}
+
+top.sort((a, b) => b.salary - a.salary);
+
+console.log(top);
+
+document.getElementById("app").innerHTML = `<h2>Top 5 Earners:</h2>`;
+for (let i = 0; i < 5; i++) {
+  document.getElementById("app").innerHTML += `<h3>${i + 1}. ${top[i].name} - Salary: ${top[i].salary}</h3>`;
+}
